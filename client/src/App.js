ed1cad0e8fbadc70ced92da2c9a2bb01db6a64d2
rhaissamrv <mrvrhai@gmail.com>
2021-05-17 11:11:18 -0600

    changes on ListingForm and App.css to fix re--render error and adjust header size to show the form on the screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
-
 import './App.css';
+import ListingForm from './components/ListingForm';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         {/* <a
           className="App-link"
         > */}
           {/* This will be our MERN Project
         </a> */}
       </header>
+      <ListingForm />
     </div>
   );
 }
---
+++ client/src/components/ListingForm.js
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+
+const ListingForm = () => {
+  const [listingCategory, setListingCategory] = useState(" ");
+  const [listingType, setListingType] = useState(" ");
+  const [listingDisposal, setListingDisposal] = useState(" ");
+  const [listingLocationPC, setListingLocationPC] = useState(" ")
+  const [listingDescription, setListingDescription] = useState(" ");
+  const [listingQuantity, setListingQuantity] = useState(0);
+
+  const categories = {
+    category1: 'Paper',
+    category2: 'Plastic',
+    category3: 'Electronic',
+    category4: 'Glass',
+    category5: 'Metal',
+    category6: 'Furniture',
+    category7: 'Textiles',
+    category8: 'Other'
+  };
+
+  const categoryInsertedHandler = (event) => {
+    setListingCategory(event.target.value);
+  };
+
+  const typeInsertedHandler = (event) => {
+    setListingType(event.target.value);
+  };
+
+  const disposalInsertedHandler = (event) => {
+    setListingDisposal(event.target.value);
+  };
+
+  const locationPCInsertedHandler = (event) => {
+    setListingLocationPC(event.target.value);
+  };
+
+  const descriptionInsertedHandler = (event) => {
+    setListingDescription(event.target.value);
+  };
+
+  const quantityInsertedHandler = (event) => {
+    setListingQuantity(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+  
+  // props.onSaveListingData();
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="add-listing__controls">
+        <div className="add-listing__control">
+          <label>Category</label>
+          <select 
+            value={listingCategory} 
+            onChange={categoryInsertedHandler}>
+                <option value={categories.category1}>Paper</option>
+                <option value={categories.category2}>Plastic</option>
+                <option value={categories.category3}>Electronic</option>
+                <option value={categories.category4}>Glass</option>
+                <option value={categories.category5}>Metal</option>
+                <option value={categories.category6}>Furniture</option>
+                <option value={categories.category7}>Textiles</option>
+                <option value={categories.category8}>Other</option>
+            </select>
+        </div>
+        <div className="add-listing__control">
+          <label>Type</label>
+          <input
+            type="text"
+            value={listingType}
+            onChange={typeInsertedHandler}
+          ></input>
+        </div>
+        <div className="add-listing__control">
+          <label>How do you want to dispose your items?</label>
+          <select
+            type="radio"
+            value={listingDisposal}
+            onChange={disposalInsertedHandler}
+            >            
+            <option value='pick-up'>Pick up</option>
+            <option value='drop-off'>Drop-off</option>
+          </select>
+        </div>
+        <div className="add-listing__control">
+          <label>Location</label>
+          <input
+            type="text"
+            value={listingLocationPC}
+            onChange={locationPCInsertedHandler}
+            ></input>
+        </div>       
+        <div className="add-listing__control">
+          <label>Please, provide a description of your item</label>
+          <input
+            type="text"
+            value={listingDescription}
+            onChange={descriptionInsertedHandler}
+          ></input>
+        </div>
+        <div className="add-listing__control">
+          <label>Quantity</label>
+          <input
+            type="number"
+            value={listingQuantity}
+            onChange={quantityInsertedHandler}
+            ></input>
+          <div className="add-listing__actions">
+            <button type="submit">Add Listing</button>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default ListingForm;
